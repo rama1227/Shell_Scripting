ee7b0406354a9788f6d655e2a566f0b9017aca3a
Kodhanda Ramayya Bobbadi <kodhanda.ramayya@kyndryl.com>
2022-04-25 12:49:45 +0530

    5

--- sample.sh
+++ sample.sh
@@ -4,7 +4,7 @@ echo "My current Path is :" $Current_Path
 Server_Uptime=`uptime`
 echo "Uptime is : " $Server_Uptime
 cat /etc/*release
-yum install nginx -y
+# yum install nginx -y
 systemctl status nginx
 systemctl start nginx
 curl ifconfig.co
