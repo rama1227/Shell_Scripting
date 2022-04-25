5071bd508018bb0163d5d968342f597d7ff8e957
Kodhanda Ramayya Bobbadi <kodhanda.ramayya@kyndryl.com>
2022-04-25 13:15:17 +0530

    7

--- sample.sh
+++ sample.sh
@@ -4,18 +4,23 @@ echo "My current Path is :" $Current_Path
 Server_Uptime=`uptime`
 echo "Uptime is : " $Server_Uptime
 cat /etc/*release 
-apk update
-apk -i add nginx             #   --> alpine linux commands.
-rc-update add nginx default    # restarts on reboot
+#apk update
+#apk -i add nginx             #   --> alpine linux commands.
+#rc-update add nginx default    # restarts on reboot
 
 # /etc/init.d/nginx start
 # or
 # rc-service nginx start
 # or
-service nginx start
+#service nginx start
 
 # apk -i upgrade
 # To install a htop package, run:
 # apk add htop
+apk del nginx
+
+apk update
+apk upgrade
+
 curl ifconfig.co
 ifconfig -a
